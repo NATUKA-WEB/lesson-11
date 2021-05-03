964c00fe61013a278e839fbde5098e460f913bd4
utrugashvili-natia <82056345+utrugashvili-natia@users.noreply.github.com>
2021-05-04 03:56:32 +0400

    paragraphs from 1 to 7

---
+++ script.js
@@ -0,0 +1,62 @@
+// პარაგრაფი-1
+
+let cars=['audi', 'bmw', 'lexux', 'volkswagen', 'porsche'];
+
+for (let i=0; i<5; i++) {
+
+    console.log(cars[2,3,4]);
+}
+
+// პარაგრაფი-2
+
+let names=['martha', 'maya', 'nora', 'nitsa', 'sani', 'nana'];
+console.log(names[4, 5])
+
+// პარაგრაფი-3
+
+let numbers=[2, 3, 4, 5, 6, 7];
+console.log(numbers[1]);
+console.log(numbers[3]);
+console.log(numbers[5]);
+console.log(numbers[0]);
+console.log(numbers[2]);
+console.log(numbers[4]);
+
+// პარაგრაფი-4
+
+
+
+
+
+
+
+
+// პარაგრაფი-5
+
+// var person= {
+//     firstname: 'John'
+//     lastname: 'Doe'
+//     age: 50
+//     eyecolor: 'blue'
+
+// }
+
+// console.log(person.eyecolor);
+// console.log(person['firstname']);
+
+
+// პარაგრაფი-6
+
+let days=['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+console.log(days[0]);
+console.log(days[1]);
+console.log(days[2]);
+console.log(days[3]);
+console.log(days[days.length-1]);
+
+
+// პარაგრაფი-7
+
+
+
+
